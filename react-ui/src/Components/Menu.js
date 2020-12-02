30bd3402d297681b69c1cf40f93fe4808a6daa9b
Arnaud <arnaud.emprin@gmail.com>
2020-12-02 22:05:34 +0100

    prod version

--- react-ui/src/Components/Menu.js
+++ react-ui/src/Components/Menu.js
@@ -11,10 +11,12 @@ class Menu extends React.Component{
         return(
             <div className="d-flex">
               <div>
+                {/* 
                 <Paper className="mr-2" id="menuCard" elevation={5} onClick={this.props.displayGenerator} >
                   <BrainSVG className="mt-4" width="150" height="150" />
                 </Paper>
                 <span id="cardTitle">Generate original artworks</span>
+                */}
               </div>
               <div>
                 <Paper className="ml-2 mr-2" id="menuCard" elevation={5} onClick={this.props.displayEmotionPicker} >
@@ -23,10 +25,12 @@ class Menu extends React.Component{
                 <span id="cardTitle">Help us improve our AI</span>
               </div>
               <div>
+                {/* 
                 <Paper className="ml-2" id="menuCard" elevation={5} onClick={this.props.displayAboutPage} >
                   <IdeaSVG className="mt-4" width="150" height="150" />
                 </Paper>
                 <span id="cardTitle">Learn about Neurogram</span>
+                */}
               </div>
             </div>
         )
