87a6c8e150a6448c6c700c2333c8df2488d148f2
Arnaud <arnaud.emprin@gmail.com>
2020-12-02 21:56:21 +0100

    trying to install modules from code

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install(torch)
+install(torchvision)
+install(matplotlib)
+install(os)
+install(random)
+install(glob)
+install(datetime)
+install(numpy)
+install(PIL)
+install(requests)
+install(errno)
+
 import torch
 from torch import nn
 import torchvision.utils as vutils
