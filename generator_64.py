c0a41edf901df53f799941eea06865befac0e098
Arnaud <arnaud.emprin@gmail.com>
2020-12-03 09:58:05 +0100

    lightnen the repo to reduce slug size

--- generator_64.py
+++ generator_64.py
@@ -2,13 +2,8 @@ import torch
 from torch import nn
 import torchvision.utils as vutils
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
-import os, random, glob, sys
-from datetime import date
+import os, sys
 import numpy as np
-from PIL import Image
-import requests
-import errno
 
 print('blabla')
 
